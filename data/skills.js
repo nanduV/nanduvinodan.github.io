7f3271bf5fb63f4bbb99a51245320ad1749a6af7
Nandu Vinodan <nanduvinodan2@gmail.com>
2020-09-13 15:47:14 +0530

    Using name icon format for skills.

--- data/skills.js
+++ data/skills.js
@@ -1,45 +1,129 @@
 const SkillsData = [
     {
-        "category": "Interests",
-        "items": [
-            "Data Structures",
-            "Algorithms",
-            "Distributed Systems",
-            "Scalabality",
-            "Computer Networking",
-            "Problem Solving",
-            "Competitive programming",
-            "Agile Methodology",
-            "Mentoring"
+        "category" : "Interests",
+        "items" : [
+            {
+                "name" : "Data Structures",
+                "icon" : ""
+            },
+            {
+                "name" : "Algorithms",
+                "icon" : ""
+            },
+            {
+                "name" : "Distributed Systems",
+                "icon" : ""
+            },
+            {
+                "name" : "Scalabality",
+                "icon" : ""
+            },
+            {
+                "name" : "Computer Networking",
+                "icon" : ""
+            },
+            {
+                "name" : "Problem Solving",
+                "icon" : ""
+            },
+            {
+                "name" : "Competitive programming",
+                "icon" : ""
+            },
+            {
+                "name" : "Agile Methodology",
+                "icon" : ""
+            },
+            {
+                "name" : "Mentoring",
+                "icon" : ""
+            }
         ]
     },
     {
-        "category": "Tools",
-        "items": [
-            "Docker",
-            "Xcode",
-            "Qt",
-            ".NET Framework",
-            "Linux",
-            "AWS",
-            "Envoy",
-            "Kubernetes",
-            "Jenkins",
-            "Kafka",
-            "Grafana"
+        "category" : "Tools",
+        "items" : [
+            {
+                "name" : "Docker",
+                "icon" : ""
+            },
+            {
+                "name" : "Xcode",
+                "icon" : ""
+            },
+            {
+                "name" : "Xcode",
+                "icon" : ""
+            },
+            {
+                "name" : ".NET Framework",
+                "icon" : ""
+            },
+            {
+                "name" : "Linux",
+                "icon" : ""
+            },
+            {
+                "name" : "AWS",
+                "icon" : ""
+            },
+            {
+                "name" : "Envoy",
+                "icon" : ""
+            },
+            {
+                "name" : "Kubernetes",
+                "icon" : ""
+            },
+            {
+                "name" : "Jenkins",
+                "icon" : ""
+            },
+            {
+                "name" : "Kafka",
+                "icon" : ""
+            },
+            {
+                "name" : "Grafana",
+                "icon" : ""
+            }
         ]
     },
     {
-        "category": "Languages",
-        "items": [
-            "C++",
-            "Java",
-            "C",
-            "Python",
-            "Go",
-            "C#",
-            "VB.NET",
-            "Bash"
+        "category" : "Languages",
+        "items" : [
+            {
+                "name" : "C++",
+                "icon" : ""
+            },
+            {
+                "name" : "Java",
+                "icon" : ""
+            },
+            {
+                "name" : "C",
+                "icon" : ""
+            },
+            {
+                "name" : "Python",
+                "icon" : ""
+            },
+            {
+                "name" : "Go",
+                "icon" : ""
+            },
+            {
+                "name" : "C#",
+                "icon" : ""
+            },
+            {
+                "name" : "VB.NET",
+                "icon" : ""
+            },
+            {
+                "name" : "Bash",
+                "icon" : ""
+            }
         ]
     }
 ]
